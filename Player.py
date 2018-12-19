085c85f28782d080ab348e26631b404011255cdf
Frank <Frank@FWs-MacBook-Air.local>
2018-12-19 00:39:59 -0800

    Updated getters and setters

--- Player.py
+++ Player.py
@@ -1,34 +1,48 @@
 class Player:
-    def __init__(self, name, team, position, minutes=0, stats=0, price=0, proj_points=0, image=None):
+    def __init__(self, name, team, position, minutes=0, stats=[0,0,0,0,0,0,0], image=None):
         self.name = name
         self.team = team
         self.position = position
-        self.minutes = minutes
+        self.avg_minutes = minutes
         self.expected_minutes = minutes
-        self.stats = stats
-        self.price = price
-        self.proj_points = proj_points
+        self.avg_stats = stats
+        self.expected_stats = stats
+        self.price = 0
+        self.proj_points = 0
         self.image = image
     def get_name(self):
         return self.name
     def get_team(self):
         return self.team
-    def get_minutes(self):
-        return self.minutes
-    def set_minutes(self, minutes):
-        self.minutes = minutes
-    def expected_minutes_change(self, new_minutes):
-        self.expected_minutes = new_minutes
+    def get_position(self):
+        return self.position
+    def set_position(self, position):
+        self.position=position
+    def get_expected_minutes(self):
+        return self.expected_minutes
+    def set_minutes(self, proj_minutes):
+        self.expected_minutes = proj_minutes
     def get_stats(self):
-        return self.stats
-    def set_stats(self, stats):
-        self.stats = stats
+        return self.expected_stats
+    def set_stats(self, proj_stats):
+        self.expected_stats = proj_stats
     def get_price(self):
         return self.price
     def set_price(self, price):
         self.price = price
     def get_proj_points(self):
         return self.proj_points
-    def set_proj_points(self, proj_points):
-        self.proj_points = proj_points
+    def set_proj_points(self, stats):
+        self.generate_projection(stats)
+    def generate_projection(self, stats):
+        doubles=0
+        doubles_bonus=0
+        for x in stats[:5]:
+            if x >= 10:
+                doubles+=1
+        if doubles>=3:
+            doubles_bonus = 4.5
+        elif double==2:
+            doubles_bonus = 1.5
+        self.proj_points = stats[0] + 1.25*stats[1] + 1.5*stats[2] + 2*stats[3] + 2*stats[4] + 0.5*stats[5] - 0.5*stats[6] + doubles_bonus
 
