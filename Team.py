2dc0ab56efc0cbbca266bbba41c698c8e3ce7a33
Frank <Frank@FWs-MacBook-Air.local>
2018-12-20 02:41:32 -0800

    Team class variables modified

--- Team.py
+++ Team.py
@@ -1,11 +1,12 @@
 class Team:
-    def __init__(self, names, possessions=0, points=0, rebounds=0):
+    def __init__(self, names, possessions=0, points=0, rebounds=0, turnovers=0):
         self.names = names
         self.possessions = possessions
         self.points_allowed = points
-        self.rebounds_allowed = rebounds
+        self.rebound_percentage = rebounds
+        self.turnovers = turnovers
     def get_names(self):
-        return self.name
+        return self.names
     def get_possessions(self):
         return self.possessions
     def set_possessions(self, possessions):
@@ -14,7 +15,7 @@ class Team:
         return self.points_allowed
     def set_points_allowed(self, points):
         self.points_allowed = points
-    def get_rebounds_allowed(self):
-        return self.rebounds_allowed
-    def set_rebounds_allowed(self, rebounds):
-        self.rebounds_allowed = rebounds
+    def get_rebound_percentage(self):
+        return self.rebound_percentage
+    def set_rebound_percentage(self, rebounds):
+        self.rebound_percentage = rebounds
