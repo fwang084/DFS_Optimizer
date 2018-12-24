8414f3e18e6f2a5a1343e1376e3c4a1175052495
Frank <Frank@FWs-MacBook-Air.local>
2018-12-24 13:31:13 -0800

    Comments added for find_best_value()

--- Calculate.py
+++ Calculate.py
@@ -8,6 +8,10 @@ for p in player_list:
     conn.execute("INSERT INTO daily_players VALUES (?, ?, ?)", (p.get_name(), p.get_proj_score(), p.get_price()))
 
 def find_best_value():
+    """
+    Finds the best projected score per price players
+    :return: A list of 40 tuples, with each tuple consisting of player name and 1000 * proj_score/price
+    """
     print(conn.execute("SELECT name, 1000 * proj_score/price FROM daily_players ORDER BY -proj_score/price LIMIT 40;").fetchall())
 find_best_value()
 
