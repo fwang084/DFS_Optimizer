a928f5232648d5f01ae640e5b7230dc9761ac811
Frank <Frank@FWs-MacBook-Air.local>
2018-12-24 13:08:51 -0800

    players added to daily_players table from player_list

--- Calculate.py
+++ Calculate.py
@@ -4,4 +4,7 @@ import sqlite3
 conn = sqlite3.Connection("n.db")
 conn.execute("DROP TABLE IF EXISTS daily_players;")
 conn.execute("CREATE TABLE daily_players(name, proj_score, price);")
+for p in player_list:
+    conn.execute("INSERT INTO daily_players VALUES (?, ?, ?)", (p.get_name(), p.get_proj_score(), p.get_price()))
+print(conn.execute("SELECT * FROM daily_players;").fetchall())
 
