a9d1a4e505933f88a53fde1721f3d6c54a75d9b5
Frank <Frank@FWs-MacBook-Air.local>
2018-12-24 12:29:15 -0800

    daily_players table created using sqlite

--- Calculate.py
+++ Calculate.py
@@ -1,1 +1,7 @@
 from Daily_Scrape import *
+import sqlite3
+
+conn = sqlite3.Connection("n.db")
+conn.execute("DROP TABLE IF EXISTS daily_players;")
+conn.execute("CREATE TABLE daily_players(name, proj_score, price);")
+
