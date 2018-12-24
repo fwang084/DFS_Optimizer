b9b3705351ef62b75b9f8b6971f54086c0b97c6c
Frank <Frank@FWs-MacBook-Air.local>
2018-12-24 13:29:39 -0800

    find_best_value() completed in Calculate

--- Calculate.py
+++ Calculate.py
@@ -6,5 +6,8 @@ conn.execute("DROP TABLE IF EXISTS daily_players;")
 conn.execute("CREATE TABLE daily_players(name, proj_score, price);")
 for p in player_list:
     conn.execute("INSERT INTO daily_players VALUES (?, ?, ?)", (p.get_name(), p.get_proj_score(), p.get_price()))
-print(conn.execute("SELECT * FROM daily_players;").fetchall())
+
+def find_best_value():
+    print(conn.execute("SELECT name, 1000 * proj_score/price FROM daily_players ORDER BY -proj_score/price LIMIT 40;").fetchall())
+find_best_value()
 
