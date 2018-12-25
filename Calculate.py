31057c2d207ea32cb00583decbe4ccb4df51fea5
Frank <Frank@FWs-MacBook-Air.local>
2018-12-25 00:01:20 -0800

    case where there are no more remaining players addressed for optimal_lineup()

--- Calculate.py
+++ Calculate.py
@@ -14,9 +14,19 @@ def find_best_value():
     """
     print(conn.execute("SELECT name, 1000 * proj_score/price FROM daily_players ORDER BY -proj_score/price LIMIT 40;").fetchall())
 
-def optimal_lineup():
+def optimal_lineup(remaining_players, lineup):
     """
     Tree recursive method to find the optimal lineup given a player budget of $50000
+    :param remaining_players: players that can still be added to the lineup
+    :param lineup: players that are already added to the lineup
     :return: list of players in the order [PG, SG, G, SF, PF, F, C, UTIL]
     """
+    if remaining_players == []:
+        for i in lineup:
+            if i is None:
+                return 0
+        return lineup
+
+
+
 
